79dfec7b0938c040a035a8aadc74a2d7d3aa2af3
DaCondor54 <enrique.llere@gmail.com>
2024-04-09 14:00:38 -0400

    Added interfaces for bird

---
+++ common/tables/BirdSpecies.ts
@@ -0,0 +1,12 @@
+export enum SpeciesStatus {
+    Vulnerable = 'Vulnérable',
+    Unthreatened = 'Non menacée',
+    MinorConcern = 'Préoccupation mineure',
+}   
+
+export interface BirdSpecies {
+    scientificName: string;
+    commonName: string;
+    speciesStatus: SpeciesStatus;
+    scientificNameConsumed: string;
+}
